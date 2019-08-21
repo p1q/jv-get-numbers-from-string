a1331dc83d0c98db37afe9725d649fac03e9e0be
AdminPC\Admin <admin2k7@gmail.com>
2019-08-21 15:03:08 +0300

    3.1 task added

--- src/main/java/core/basesyntax/FindNumbers.java
+++ src/main/java/core/basesyntax/FindNumbers.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+
 public class FindNumbers {
 
     /**
@@ -10,9 +12,49 @@ public class FindNumbers {
      * массив, каждый элемент этого массива умножить на 2. Метод должен возвращать этот массив.</p>
      *
      * <p>Пример: если дана строка "data 48 call 9 read13 blank0a", то в массиве должны оказаться
-     * числа 92, 18, 26 и 0.</p>
+     * числа 96, 18, 26 и 0.</p>
      */
     public int[] getAllNumbers(String text) {
-        return null;
+        int[] tempArray = new int[text.length()];
+        int tempArrayCurrentIndex = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            if (isDigit(text.substring(i, i + 1))) {
+                int tmp = Integer.valueOf(text.substring(i, i + countDigitsQuantity(text.substring(i))));
+
+                if (i > 0 && text.charAt(i - 1) == '-') {
+                    tempArray[tempArrayCurrentIndex] = -tmp * 2;
+                } else {
+                    tempArray[tempArrayCurrentIndex] = tmp * 2;
+                }
+
+                tempArrayCurrentIndex++;
+                i += countDigitsQuantity(text.substring(i));
+            }
+        }
+        return Arrays.copyOf(tempArray, tempArrayCurrentIndex);
+    }
+
+    private boolean isDigit(String subString) {
+        if (subString != null && !subString.isEmpty()) {
+            String digits = "0123456789";
+            return digits.contains(subString);
+        } else {
+            return false;
+        }
+    }
+
+    private int countDigitsQuantity(String subString) {
+        int counter = 0;
+        int i = 0;
+
+        while (isDigit(subString.substring(i, i + 1))) {
+            counter++;
+            i++;
+            if (i == subString.length()) {
+                break;
+            }
+        }
+        return counter;
     }
 }
